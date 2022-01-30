621b72d903464c7bcf85942ca810b2b60c3d2144
akilansengottaiyan <akilan0306@gmail.com>
2022-01-30 19:09:07 +0530

    app base setup with truffle

---
+++ truffle-config.js
@@ -0,0 +1,58 @@
+const path = require('path')
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+require('dotenv').config()
+module.exports = {
+  contracts_build_directory: path.join(__dirname, 'front_end/src/contracts'),
+  plugins: ['truffle-contract-size'],
+  networks: {
+    development: {
+      host: '127.0.0.1',
+      port: 7545,
+      network_id: '*'
+    },
+    'mainnet-fork': {
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: 1,
+      skipDryRun: true,
+      chainlinkConfig: {
+        priceFeed: '0x5f4ec3df9cbd43714fe2740f5e3616155c5b8419'
+      }
+    },
+    rinkeby: {
+      provider: () =>
+        new HDWalletProvider(
+          process.env.MNEMONIC,
+          `https://rinkeby.infura.io/v3/${process.env.INFURA_PROJECT_ID}`
+        ),
+      network_id: 4,
+      gas: 5500000,
+      confirmations: 0,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+    kovan: {
+      provider: () =>
+        new HDWalletProvider(
+          process.env.MNEMONIC,
+          `https://kovan.infura.io/v3/${process.env.INFURA_PROJECT_ID}`
+        ),
+      network_id: 42,
+      gas: 5500000,
+      confirmations: 0,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    }
+  },
+  compilers: {
+    solc: {
+      version: '^0.8.0',
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 1
+        }
+      }
+    }
+  }
+}
